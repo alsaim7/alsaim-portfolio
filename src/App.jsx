9a361d79fe9d90539140234ea85ae8e5f6f14777
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-03-08 00:23:10 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Header } from './components/header/Header'
 import { Home } from './components/home/Home'
-import { About } from './components/about/about'
+import { About } from './components/about/About'
 import { Skills } from './components/skills/Skills'
 import { Projects } from './components/projects/Projects'
 import { Contact } from './components/contact/Contact'
