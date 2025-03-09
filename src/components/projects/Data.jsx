932b38c0bfdc72b7cbda55395772df1cef5fca49
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-03-10 03:35:45 +0530

    added modals

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -3,6 +3,9 @@ export const projectsData = [
         id: 1,
         image: 'https://res.cloudinary.com/dnkqy2kkr/image/upload/v1741365683/img1_wgxfiz.png',
         title: "Prime Tutors",
+        fullTitle:'Prime Tutors',
+        description:'Prime Tutors is a fully responsive static website designed with smooth animations for an engaging user experience. The site features a contact form that seamlessly integrates with a spreadsheet, automatically updating submitted data while also sending email notifications. This ensures efficient communication and data management. Developed as a freelance project, Prime Tutors showcases a clean, modern design with optimized performance.',
+        tech: 'React Js, Gsap for animations, Bootstrap.',
         category: "Web",
         link: 'https://primetutors.netlify.app/'
     },
@@ -10,6 +13,9 @@ export const projectsData = [
         id: 2,
         image: 'https://res.cloudinary.com/dnkqy2kkr/image/upload/v1741366600/img2_ek9fyt.png',
         title: 'AS-Camp',
+        fullTitle: 'AS-Camp: A Full-Stack Campground Exploration and Sharing Platform',
+        description:'AS-Camp is a full-stack web application for discovering, sharing, and reviewing campgrounds. Users can post their own campsites, explore detailed locations, and leave comments. Built with Node.js, Express.js, MongoDB, and Map APIs, it features a responsive design and secure authentication.',
+        tech: "Express.js, MongoDB, Mongoose, Passport.js, Cloudinary, MapTiler, EJS, Joi, Express-Session, Helmet.",
         category: "Web",
         link: 'https://github.com/alsaim7/AS-Camp.git'
     },
@@ -17,6 +23,9 @@ export const projectsData = [
         id: 3,
         image: 'https://res.cloudinary.com/dnkqy2kkr/image/upload/v1741369118/img3_py3fmm.png',
         title: "AlertSphere",
+        fullTitle: 'AlertSphere: Disaster Tweet Detector',
+        description: 'AlertSphere is a machine learning-based system designed to predict whether a given tweet indicates a disaster. It utilizes natural language processing (NLP) techniques to analyze the text content of tweets and classify them as either disaster-related or not.',
+        tech: 'Python, NLP, Machine Learning, Scikit-learn, pandas, matplotlib, tkinter',
         category: "ML/DS",
         link: 'https://github.com/alsaim7/AlertSphere.git'
     },
@@ -24,6 +33,9 @@ export const projectsData = [
         id: 4,
         image: 'https://res.cloudinary.com/dnkqy2kkr/image/upload/v1741369497/img4_eti2mi.png',
         title: "Forecast Flow",
+        fullTitle: 'Forecast Flow: Rainfall predicton model',
+        description: 'ForecastFlow is a machine learning-based system designed to predict rainfall based on historical weather data. It utilizes data analysis and machine learning techniques to analyze various factors influencing rainfall and predict whether it will rain tomorrow.',
+        tech: 'Python, Machine Learning, Scikit-learn, pandas, matplotlib, tkinter',
         category: "ML/DS",
         link: 'https://github.com/alsaim7/ForecastFlow.git'
     },
@@ -31,6 +43,9 @@ export const projectsData = [
         id: 5,
         image: 'https://res.cloudinary.com/dnkqy2kkr/image/upload/v1741370384/img5_a1iamq.png',
         title: "ShigotoZen: To-do list",
+        fullTitle: 'ShigotoZen: A Simple and Efficient To-Do List App',
+        description: 'ShigotoZen: A Simple and Efficient To-Do List App built with React and Material UI, featuring local storage for task persistence and a clean, responsive UI. This project demonstrates modern front-end development practices and provides a straightforward way to manage tasks.',
+        tech: 'React Js, Material UI',
         category: "Web",
         link: 'https://shigotozen.netlify.app/'
     },
--- src/components/projects/WorkItems.jsx
+++ src/components/projects/WorkItems.jsx
@@ -1,11 +1,70 @@
-export function WorkItems({item}){
-    return(
+import Modal from "react-modal"
+import { useState, useEffect } from "react"
+import Button from '@mui/material/Button';
+
+Modal.setAppElement('#root')
+export function WorkItems({ item }) {
+    const [modalOpen, setModalOpen] = useState(false)
+
+    const [modalWidth, setModalWidth] = useState(window.innerWidth < 600 ? '80%' : '50%')
+
+    // Update modal width on window resize
+    useEffect(() => {
+        const handleResize = () => {
+            setModalWidth(window.innerWidth < 600 ? '80%' : '50%')
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+
+    return (
         <div className="work__card">
-            <img src={item.image} alt="" className="work__img"/>
+            <img src={item.image} alt="" className="work__img" />
             <h3 className="work__title">{item.title}</h3>
-            <a href={item.link} className="work__button">
-                Check it out <i className="bx bx-right-arrow-alt work__button-icon"></i>
+
+            <a className="work__button details-button" onClick={() => setModalOpen(true)}>
+                Details <i className="bx bx-right-arrow-alt work__button-icon details-icon"></i>
             </a>
+
+
+            <a href={item.link} className="work__button">
+                View Project <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            </a>
+
+
+            <Modal
+                isOpen={modalOpen}
+                onRequestClose={() => setModalOpen(false)}
+                style={{
+                    content: {
+                        width: modalWidth,
+                        height: 'fit-content',
+                        margin: 'auto',
+                        borderRadius: '10px',
+                        padding: '30px',
+                        backgroundColor: 'var(--container-color)',
+                        overflowY: 'hidden',
+                    },
+                    overlay: {
+                        background: 'rgba(0,0,0,0.5)'
+                    }
+                }}
+            >
+                <div className="modal-content">
+                    <div>
+                        <h3 className="modal-title">{item.fullTitle}</h3>
+                        <p className="modal-des"><b>Description: </b>{item.description}</p>
+                        <p className="modal-tech"><b>Technologies used: </b>{item.tech}</p>
+                    </div>
+
+                    <Button className="modal-button" variant="contained" color="error" onClick={(() => {
+                        setModalOpen(false)
+                    })}><i className="uil uil-times"></i></Button>
+                </div>
+            </Modal>
+
         </div>
     )
 }
