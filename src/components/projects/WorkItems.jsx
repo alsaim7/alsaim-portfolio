3fd4d8424b11c98243e322c8bd75e9e87a2bebb1
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-03-11 01:14:07 +0530

    modal fix

--- src/components/projects/WorkItems.jsx
+++ src/components/projects/WorkItems.jsx
@@ -1,30 +1,48 @@
-import Modal from "react-modal"
 import { useState, useEffect } from "react"
 import Button from '@mui/material/Button';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
 
-Modal.setAppElement('#root')
+
+
 export function WorkItems({ item }) {
-    const [modalOpen, setModalOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    const [modalWidth, setModalWidth] = useState(window.innerWidth < 600 ? '80%' : '50%')
+    const [modalWidth, setModalWidth] = useState(window.innerWidth < 900 ? '80%' : '50%')
 
     // Update modal width on window resize
     useEffect(() => {
         const handleResize = () => {
-            setModalWidth(window.innerWidth < 600 ? '80%' : '50%')
+            setModalWidth(window.innerWidth < 900 ? '80%' : '50%')
         }
 
         window.addEventListener('resize', handleResize)
         return () => window.removeEventListener('resize', handleResize)
     }, [])
 
-
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: modalWidth,
+        bgcolor: 'var(--container-color)',
+        border: '2px solid var(--title-color)',
+        borderRadius: '10px',
+        boxShadow: 24,
+        p: 4,
+    };
     return (
         <div className="work__card">
             <img src={item.image} alt="" className="work__img" />
             <h3 className="work__title">{item.title}</h3>
 
-            <button className="work__button details-button" onClick={() => setModalOpen(true)}>
+            <button className="work__button details-button" onClick={handleOpen}>
                 Details <i className="bx bx-right-arrow-alt work__button-icon details-icon"></i>
             </button>
 
@@ -35,34 +53,35 @@ export function WorkItems({ item }) {
 
 
             <Modal
-                isOpen={modalOpen}
-                onRequestClose={() => setModalOpen(false)}
-                style={{
-                    content: {
-                        width: modalWidth,
-                        height: 'fit-content',
-                        margin: 'auto',
-                        borderRadius: '10px',
-                        padding: '30px',
-                        backgroundColor: 'var(--container-color)',
-                        overflowY: 'hidden',
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
                     },
-                    overlay: {
-                        background: 'rgba(0,0,0,0.5)'
-                    }
                 }}
             >
-                <div className="modal-content">
-                    <div>
-                        <h3 className="modal-title">{item.fullTitle}</h3>
-                        <p className="modal-des"><b>Description: </b>{item.description}</p>
-                        <p className="modal-tech"><b>Technologies used: </b>{item.tech}</p>
-                    </div>
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography id="transition-modal-title modal-title" variant="h6" component="h2" sx={{ color: 'var(--title-color)' }}>
+                            {item.fullTitle}
+                        </Typography>
+                        <Typography id="transition-modal-description" sx={{ mt: 2, color: 'var(--text-color)' }}>
+                            <b>Description: </b>{item.description}
+                        </Typography>
+                        <Typography id="transition-modal-description modal-tech" sx={{ mt: 2, color: 'var(--text-color)' }}>
+                            <b>Technologies used: </b>{item.tech}
+                        </Typography>
 
-                    <Button className="modal-button" variant="contained" color="error" onClick={(() => {
-                        setModalOpen(false)
-                    })}><i className="uil uil-times"></i></Button>
-                </div>
+                        <Button className="modal-button" variant="contained" color="error" onClick={handleClose
+                        } sx={{mt:2, fontSize: '10px'}}><i className="uil uil-times close-icon"></i></Button>
+                    </Box>
+
+                </Fade>
             </Modal>
 
         </div>
