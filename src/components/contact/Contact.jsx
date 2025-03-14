1ebdb677aa51d59e63dc1602d4f1982fb003836e
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-03-15 01:50:23 +0530

    adding animation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -56,7 +56,7 @@ export function Contact() {
         return(()=>{
             mm.revert()
         })
-    })
+    },[])
 
     return (
         <section className="contact section" id="contact">
