02f46dc009f5e91f536a0a4d6c645242cdcfa8a8
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-03-22 19:33:57 +0530

    animation fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,8 @@ import { Projects } from './components/projects/Projects'
 import { Contact } from './components/contact/Contact'
 import { Footer } from './components/footer/Footer'
 import { ScrollUp } from './components/scrollup/ScrollUp'
-import { useEffect } from 'react'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 function App() {
-
-  useEffect(() => {
-    // function refreshScrollTrigger() {
-    //   setTimeout(() => {
-    //     ScrollTrigger.refresh();
-    //   }, 500);
-    // }
-
-    // window.addEventListener("load", refreshScrollTrigger);
-    // document.addEventListener("DOMContentLoaded", refreshScrollTrigger);
-
-    // 👇 Force refresh after first scroll/touch
-    const handleInteraction = () => {
-      setTimeout(() => {
-        ScrollTrigger.refresh();
-      }, 300);
-      window.removeEventListener("scroll", handleInteraction);
-      window.removeEventListener("touchstart", handleInteraction);
-    };
-
-    window.addEventListener("scroll", handleInteraction);
-    window.addEventListener("touchstart", handleInteraction);
-
-    return () => {
-      // window.removeEventListener("load", refreshScrollTrigger);
-      // document.removeEventListener("DOMContentLoaded", refreshScrollTrigger);
-      window.removeEventListener("scroll", handleInteraction);
-      window.removeEventListener("touchstart", handleInteraction);
-    };
-  }, []);
 
   return (
     <>
