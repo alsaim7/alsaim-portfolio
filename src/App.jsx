d925a2d16bb205a03ac6e7be89b11e7186e4da7a
Al Saim Shakeel <alsaimshakeel45@gmail.com>
2025-03-22 19:00:51 +0530

    animation fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,41 @@ import { Projects } from './components/projects/Projects'
 import { Contact } from './components/contact/Contact'
 import { Footer } from './components/footer/Footer'
 import { ScrollUp } from './components/scrollup/ScrollUp'
+import { useEffect } from 'react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 function App() {
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        ScrollTrigger.refresh();
+      }, 500); // Delay refresh slightly
+    });
+
+    return () => {
+      window.removeEventListener("load", () => {
+        setTimeout(() => {
+          ScrollTrigger.refresh();
+        }, 500);
+      });
+    };
+  }, []);
 
   return (
     <>
       <Header />
 
       <main className='main'>
-        <Home/>
-        <About/>
-        <Skills/>
-        <Projects/>
-        <Contact/>
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
       </main>
 
-      <Footer/>
-      <ScrollUp/>
+      <Footer />
+      <ScrollUp />
     </>
   )
 }
